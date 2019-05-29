648d0694d4962aa49cf51d8c778e74ec56de3b85
redmed86 <derek.g.ross86@gmail.com>
2019-05-29 12:43:26 -0700

    added comments and made code more readable

--- tests/es6_example/ipLocalization.js
+++ tests/es6_example/ipLocalization.js
@@ -3,10 +3,12 @@ const expect = require('chai').expect;
 describe('IP Localization', function() {
 
     it("Check for Indian IP address", function() {
+        //Navigate to What's My IP page
         browser.url('https://www.whatsmyip.org/')
-        const link = $('body > div > div > main > section.bit_more_info > p:nth-child(4) > a');
-        const serverInfo = $('body > div > div > main > section:nth-child(2) > h2')
-        link.click();
+        //Find and click on more info link
+        const myInfoLink = $('body > div > div > main > section.bit_more_info > p:nth-child(4) > a');
+        myInfoLink.click();
+        //Scroll to info section
         browser.scroll('body > div > div > main > section:nth-child(2) > h2');
     });
 });
